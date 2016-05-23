a478616909dc1ea90e36ac1882c7fec8ff2f08ae
Manabu Ori <manabu.ori+github@gmail.com>
2016-05-23 17:27:13 +0900

    add build_image.sh

---
+++ scripts/build_image.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+source ~/stackrc
+workdir=~/images_work
+imagedir=~/images
+
+mkdir -p ${workdir}
+mkdir -p ${imagedir}
+
+image=CentOS-7-x86_64-GenericCloud.qcow2.xz
+mkdir -p ~/.cache/image-create
+scp ori@trevally.usersys.redhat.com:${image} ~/.cache/image-create/${image}
+
+export NODE_DIST=centos7
+export USE_DELOREAN_TRUNK=1
+export DELOREAN_TRUNK_REPO="http://trunk.rdoproject.org/centos7/current-tripleo/"
+export DELOREAN_REPO_FILE="delorean.repo"
+
+cd ${workdir} && time openstack overcloud image build --all
+mv ${workdir}/{ironic-python-agent.initramfs,ironic-python-agent.kernel,ironic-python-agent.vmlinuz,overcloud-full.initrd,overcloud-full.qcow2,overcloud-full.vmlinuz} ${imagedir}
